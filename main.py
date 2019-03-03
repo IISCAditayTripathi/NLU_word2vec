8d9aa87ca619925ea15951dec49bcec4c8584efa
Aditay Tripathi <aditay@puri.cds.iisc.ac.in>
2019-03-03 22:29:59 +0530

    training happening

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import argparse
 import pickle
 from tqdm import tqdm
 import math
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 
@@ -25,7 +26,7 @@ parser.add_argument('--initilize', type=bool, default=False)
 parser.add_argument('--embedding_dim', type=int, default=100)
 parser.add_argument('--train_embeddings', type=bool, default=True)
 parser.add_argument('--nb_negative_samples', type=int, default=5)
-parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--context', type=int, default=5)
 parser.add_argument('--lemmatize', type=bool, default=True)
 parser.add_argument('--stemming', type=bool, default=False)
@@ -40,6 +41,9 @@ nltk.download('wordnet')
 lemmer = WordNetLemmatizer()
 
 def sigmoid(x):
+    # print(x)
+    if x < -500:
+        x = -500
     return 1 / (1 + math.exp(-x))
 
 def get_train_test_splits():
@@ -150,34 +154,35 @@ def normalize_counts(count_dict):
 
 def data_generator(data_list, threshold, context, normalized_counts):
     # constant = 601000
-    constant = 1
-    for word_index in range(len(data_list)):
-        context_words = []
-        if (word_index - (context-1)/2) < 0:
-            if np.random.random_sample() > 1 - np.sqrt(threshold/(constant*normalized_counts[data_list[word_index]])):
-                word = data_list[word_index]
-                if len(data_list[0:word_index]) != 0:
-                    [context_words.append(c) for c in data_list[0:word_index]]
-                [context_words.append(c) for c in data_list[word_index+1:word_index+int((context+1)/2)]]
-                yield word, context_words
+    while True:
+        constant = 1
+        for word_index in range(len(data_list)):
+            context_words = []
+            if (word_index - (context-1)/2) < 0:
+                if np.random.random_sample() > 1 - np.sqrt(threshold/(constant*normalized_counts[data_list[word_index]])):
+                    word = data_list[word_index]
+                    if len(data_list[0:word_index]) != 0:
+                        [context_words.append(c) for c in data_list[0:word_index]]
+                    [context_words.append(c) for c in data_list[word_index+1:word_index+int((context+1)/2)]]
+                    yield word, context_words
+                else:
+                    yield None
+            elif (word_index + (context-1)/2) >= len(data_list):
+                if np.random.random_sample() > 1 - np.sqrt(threshold/(constant*normalized_counts[data_list[word_index]])):
+                    word = data_list[word_index]
+                    [context_words.append(c) for c in data_list[word_index-int((context-1)/2): word_index]]
+                    [context_words.append(c) for c in data_list[word_index+1:]]
+                    yield word, context_words
+                else:
+                    yield None
             else:
-                yield None
-        elif (word_index + (context-1)/2) >= len(data_list):
-            if np.random.random_sample() > 1 - np.sqrt(threshold/(constant*normalized_counts[data_list[word_index]])):
-                word = data_list[word_index]
-                [context_words.append(c) for c in data_list[word_index-int((context-1)/2): word_index]]
-                [context_words.append(c) for c in data_list[word_index+1:]]
-                yield word, context_words
-            else:
-                yield None
-        else:
-            if np.random.random_sample() > 1 - np.sqrt(threshold/(constant*normalized_counts[data_list[word_index]])):
-                word = data_list[word_index]
-                [context_words.append(c) for c in data_list[word_index-int((context-1)/2):word_index]]
-                [context_words.append(c) for c in data_list[word_index+1:word_index+int((context+1)/2)]]
-                yield word, context_words
-            else:
-                yield None
+                if np.random.random_sample() > 1 - np.sqrt(threshold/(constant*normalized_counts[data_list[word_index]])):
+                    word = data_list[word_index]
+                    [context_words.append(c) for c in data_list[word_index-int((context-1)/2):word_index]]
+                    [context_words.append(c) for c in data_list[word_index+1:word_index+int((context+1)/2)]]
+                    yield word, context_words
+                else:
+                    yield None
 
 
 
@@ -200,15 +205,15 @@ def get_loss(pos_samples, neg_samples, word_embedding, context_embedding, normal
     loss = sum(pos_loss_list) + sum(neg_loss_list)
     return loss
 
-def get_grad(pos_samples, neg_samles, word_embedding, context_embedding):
+def get_grad(pos_samples, neg_samples, word_embedding, context_embedding):
     word = pos_samples[0]
     context = pos_samples[1]
 
     word_grad = [(1-sigmoid(np.dot(word_embedding[word], context_embedding[c])))*word_embedding[word] for c in context]
-    context_grad = {}
-    [context_grad[c].add((1-sigmoid(np.dot(word_embedding[word], context_embedding[c])))*context_embedding[c]) for c in context]
-    neg_context_grad = {}
-    [neg_context_grad[c].add(-1*(1-sigmoid(-1*np.dot(word_embedding[word], context_embedding[c])))*context_embedding) for c in neg_samles]
+    context_grad = defaultdict(list)
+    [context_grad[c].append((1-sigmoid(np.dot(word_embedding[word], context_embedding[c])))*context_embedding[c]) for c in context]
+    neg_context_grad = defaultdict(list)
+    [neg_context_grad[c].append(-1*(1-sigmoid(-1*np.dot(word_embedding[word], context_embedding[c])))*context_embedding[c]) for c in neg_samples]
 
     return word_grad, context_grad, neg_context_grad, word, context
 
@@ -216,7 +221,7 @@ def sgd_step(embedding, grad, lr):
     pass
 
 def train_word2vec(tokenized_data_file, nb_neg_samples, lr, context, nb_epochs, dim, anneal):
-    # build_dict(tokenized_data_file)
+    build_dict(tokenized_data_file)
     train_counts = pickle.load(open('dict_count_train_lemmatized.pkl','rb'))
     normalized_counts = normalize_counts(train_counts)
     threshold = 0.00001
@@ -234,7 +239,8 @@ def train_word2vec(tokenized_data_file, nb_neg_samples, lr, context, nb_epochs, 
         running_loss = []
         print('Epoch {}/{}'.format(epoch, nb_epochs - 1))
         print('-' * 10)
-        for i in tqdm(range(data_size), unit='Words'):
+        loader = tqdm(range(data_size), unit='words')
+        for i in enumerate(loader):
             positive_samples = next(positive_samples_generator)
             negative_samples = next(negative_samples_generator)
             if positive_samples != None:
@@ -246,11 +252,14 @@ def train_word2vec(tokenized_data_file, nb_neg_samples, lr, context, nb_epochs, 
                 for w_grad in word_grad:
                     word_embedding[word] = word_embedding[word] + lr*w_grad
                 for key, c_grad in context_grad.items():
-                    context_embedding[key] = context_grad[key] + lr*c_grad
+                    # print(c_grad[0].shape)
+                    context_embedding[key] = context_embedding[key] + lr*c_grad[0]
                 for key, n_grad in neg_context_grad.items():
-                    context_embedding[key] = context_grad[key] + lr*c_grad
+                    context_embedding[key] = context_embedding[key] + c_grad[0]
                 running_loss.append(loss)
-                tqdm.set_postfix(loss=sum(loss)/len(loss))
+                loader.set_postfix(loss=sum(running_loss)/len(running_loss))
+    pickle.dump(word_embedding, open('word_embeddingsv1.pkl', 'wb'))
+    pickle.dump(context_embedding, open('context_embeddingv1.pkl', 'wb'))
 
                 # loss = get_loss(pos_samples, neg_samles)
                 # word_grad, context_grad, neg_context_grad, word, context = get_grad(positive_samples, negative_samples, word_embedding, context_embedding)
